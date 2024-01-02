b442273556824a2389614ed565bf416430d0a3c0
Yassir El yaqine <yel-yaqi@e1r6p7.1337.ma>
2024-01-02 11:10:57 +0100

    strerror and printf for error messages instead of perror

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 15:12:18 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2023/12/27 19:09:02 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/01/02 11:04:59 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,13 +17,13 @@ int	main(int argc, char **argv)
 	int	infile_fd;
 	int	outfile_fd;
 
-	if (argc == 1 || (!ft_strncmp(argv[1], argv[argc - 1], ft_strlen(argv[1])) // infile == outfile
+	if (argc <= 1 || (!ft_strncmp(argv[1], argv[argc - 1], ft_strlen(argv[1])) // infile == outfile
 			&& ft_strlen(argv[1]) == ft_strlen(argv[argc - 1]) && argc != 2))
 		exit(EXIT_SUCCESS);
 	infile_fd = open(argv[1], O_RDONLY);
 	if (infile_fd < 0)
 	{
-		perror("pipex");
+		ft_printf("pipex: %s: %s", strerror(errno), argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	if (argc == 2)
@@ -33,9 +33,9 @@ int	main(int argc, char **argv)
 		outfile_fd = open(argv[argc - 1], O_CREAT, 0644);
 	if (outfile_fd < 0)
 	{
-		perror("pipex");
+		ft_printf("pipex: %s: %s", strerror(errno), argv[argc - 1]);
 		close(infile_fd);
 		exit(EXIT_FAILURE);
 	}
-	// recursive_feeder(infile_fd, outfile_fd, argv);
+	
 }

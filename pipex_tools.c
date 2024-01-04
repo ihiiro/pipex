f79c2c476139defa55b0a20f331643bb8f20cf11
Yassir El yaqine <yel-yaqi@e3r8p5.1337.ma>
2024-01-04 16:04:52 +0100

    corrected fds[0] < 0 error printf

--- pipex_tools.c
+++ pipex_tools.c
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/27 18:31:44 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/01/04 15:26:26 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/01/04 16:02:15 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int	*unpack_fds(int *fds, int argc, char **argv, int *errnos) //perms
 	errnos[0] = errno;
 	if (argc == 2)
 		return (errnos[1] = -1, fds);
-	fds[1] = open(argv[argc - 1], O_WRONLY | O_CREAT /*| O_TRUNC */);
+	fds[1] = open(argv[argc - 1], O_WRONLY | O_CREAT | O_TRUNC);
 	return (errnos[1] = errno, fds);
 }
 
@@ -30,7 +30,7 @@ void	check_fds(int *fds, int argc, char **argv, int *errnos)
 	{
 		if (errnos[1] != -1 && fds[1] >= 0)
 			close(fds[1]);
-		ft_printf("pipex: %s: %s", strerror(errnos[0]), argv[0]);
+		ft_printf("pipex: %s: %s", strerror(errnos[0]), argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	if (errnos[1] != -1 && fds[1] < 0)

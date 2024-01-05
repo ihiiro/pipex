0dc1b22a8f844b90f1b1f72abc3eb628f5a68f5e
Yassir El yaqine <yel-yaqi@e3r1p14.1337.ma>
2024-01-05 20:15:38 +0100

    figured out how to use dup2

--- Makefile
+++ Makefile
@@ -6,7 +6,7 @@
 #    By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/12/27 18:57:52 by yel-yaqi          #+#    #+#              #
-#    Updated: 2024/01/05 18:48:14 by yel-yaqi         ###   ########.fr        #
+#    Updated: 2024/01/05 19:15:25 by yel-yaqi         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -19,8 +19,8 @@ pipex_tools = pipex_tools_0.c pipex_tools_1.c
 
 all: pipex
 
-pipex: $(liblibft) $(libprintf) $(get_next_line) main.c pipex.c $(pipex_tools)
-	cc $(CFLAGS) main.c $(pipex_tools) pipex.c $(get_next_line) $(liblibft) $(libprintf) -o $@
+pipex: $(liblibft) $(libprintf) $(get_next_line) main.c $(pipex_tools)
+	cc $(CFLAGS) main.c $(pipex_tools) $(get_next_line) $(liblibft) $(libprintf) -o $@
 
 libft/libft.a: libft
 	make -C libft
--- pipex_tools_1.c
+++ pipex_tools_1.c
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/05 18:48:45 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/01/05 19:02:38 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/01/05 20:12:20 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,29 @@
 
 void	pipeline(int *fds, int *errnos, char **argv, int argc)
 {
-	// (void)errnos;
-	// (void)argv;
+	(void)errnos;
+	(void)argv;
+	// int		i;
+	pid_t	child_pid;
+	int		pipee[2];
+
 	if (argc == 2)
-		write_fd_to_fd(fds[0], 1);
+		write_fd_to_fd(fds[0], STDOUT_FILENO);
+	else
+	{
+		pipe(pipee);
+		write_fd_to_fd(fds[0], pipee[1]);
+		child_pid = fork();
+		if (child_pid == 0)
+		{
+			dup2(pipee[0], STDIN_FILENO);
+			char	*argvv[] = {"/usr/bin/grep", "hi", NULL};
+			execve(argvv[0], argvv, NULL);
+			exit(EXIT_SUCCESS);
+		}
+		else
+		{
+			// waitpid(child_pid, NULL, 0);
+		}
+	}
 }

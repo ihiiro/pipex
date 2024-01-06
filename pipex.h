ba7c42446c2eb9c9b515ed90dd4620c688c7f084
Yassir El yaqine <yel-yaqi@e3r2p12.1337.ma>
2024-01-06 19:25:44 +0100

    cleanup

--- pipex.h
+++ pipex.h
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 15:38:46 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/01/06 19:07:19 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/01/06 19:25:30 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@
 int		*unpack_fds(int *fds, int argc, char **argv, int *errnos);
 void	check_fds(int *fds, int argc, char **argv, int *errnos);
 void	write_fd_to_fd(int fd_read, int fd_write);
-int		file_exists(char *filename);
-int		is_file(char *symbol);
 void	pipeline(int *fds, int argc, char **argv, char *paths);
 int		strictcmp(char *str0, char *str1);
 int		get_heredoc_fd(char *limiter, int *errnos);

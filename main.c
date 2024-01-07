6bb6bd13daeadd88d394d6ce39893f5ccddfcfa3
Yassir El yaqine <yel-yaqi@e3r1p14.1337.ma>
2024-01-07 15:49:36 +0100

    writing pipeline()

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/18 15:12:18 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/01/07 15:04:29 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/01/07 15:22:39 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,5 @@ int	main(int argc, char **argv, char **env)
 		argv += 3;
 	else
 		argv += 2;
-
-	int	pipe_fds[2];
-	pipe(pipe_fds);
-	char	*argvv[] = {"/bin/cat", "-e", NULL};
-	execute_cmd(fds[0], pipe_fds[WRITE_END], argvv);
-	write_fd_to_fd(pipe_fds[READ_END], 1);
 	// while (1);
 }
--- pipex_tools_2.c
+++ pipex_tools_2.c
@@ -6,7 +6,7 @@
 /*   By: yel-yaqi <yel-yaqi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/07 14:48:03 by yel-yaqi          #+#    #+#             */
-/*   Updated: 2024/01/07 14:55:20 by yel-yaqi         ###   ########.fr       */
+/*   Updated: 2024/01/07 15:48:51 by yel-yaqi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,4 +23,46 @@ int	cmp_to_stdin(char *str, char *stdin_str)
 		return (0);
 	else
 		return (1);
+}
+
+void	pipeline(int argc, char **argv, char *paths)
+{
+	char	*argvv[3];
+	char	*cmd;
+	int		pipe_fds[2];
+	int		i;
+	int		path_count;
+
+	if (pipe(pipe_fds) < 0)
+		exit(EXIT_FAILURE);
+	i = 0;
+	while (1)
+	{
+		if (!argv[i + 1])
+			break ;
+		cmd = first_word(argv[i]);
+		argvv[0] = construct_cmd(paths, cmd);
+		free(cmd);
+		argvv[1] = ft_strchr(argv[i], ' ') + 1;
+		argvv[2] = NULL;
+		if (i == 0)
+			fds[0]
+		else
+			
+		i++;
+	}
+}
+
+char	*first_word(char *str)
+{
+	char	*word;
+	int		i;
+
+	i = 0;
+	while (str[i] != ' ')
+		i++;
+	word = ft_substr(str, 0, i);
+	if (!word)
+		exit(EXIT_FAILURE);
+	return (word);
 }
